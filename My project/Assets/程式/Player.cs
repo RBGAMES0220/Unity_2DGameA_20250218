9c279a296bf34ee6983d5f27db796c7f3f688d43
RBGAMES0220 <129024364+RBGAMES0220@users.noreply.github.com>
2025-02-21 12:35:21 +0800

    20250221

--- My project/Assets/程式/Player.cs
+++ My project/Assets/程式/Player.cs
@@ -14,31 +14,71 @@ public class Player : MonoBehaviour
 
     private float xInput;
 
-    private bool isMoving;
-    
+    private int facingDir = 1;
+    private bool facingRight = true;
+   
 
-    // Start is called before the first frame update
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+        CheckInput();
+        Movement();
+        AnimatorControllers();
+        FlipController();
+       
+    }
+
+    private void CheckInput()
+    {
         xInput = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2 (xInput * moveSpeed, rb.velocity.y);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            Jump();
         }
 
-        isMoving = rb.velocity.x != 0;
+    }
+
+    private void Movement()
+    {
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+    }
+
+    private  void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void AnimatorControllers()
+    {
+        bool isMoving = rb.velocity.x != 0;
 
         anim.SetBool("isMoving", isMoving);
     }
 
-   
+   private void Flip()
+    {
+        facingDir = facingDir * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    private void FlipController()
+    {
+        if (rb.velocity.x >0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (rb.velocity.x <0 && facingRight)
+        {
+            Flip();
+        }
+    }
 }
