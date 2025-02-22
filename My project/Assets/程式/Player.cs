61bb39e137744eec7192818ef0a9b3842aafe443
RBGAMES0220 <129024364+RBGAMES0220@users.noreply.github.com>
2025-02-22 12:19:30 +0800

    20250222

--- My project/Assets/程式/Player.cs
+++ My project/Assets/程式/Player.cs
@@ -74,7 +74,10 @@ public class Player : MonoBehaviour
     {
         bool isMoving = rb.velocity.x != 0;
 
+        anim.SetFloat("yVelocity" , rb.velocity.y);
+
         anim.SetBool("isMoving", isMoving);
+        anim.SetBool("isGrounded", isGrounded);
     }
 
    private void Flip()
